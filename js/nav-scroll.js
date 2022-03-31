6e88fb21a2e55cbbc0e17f73e3d60220a3891f88
WyattHWilliams <whwilliams.library@gmail.com>
2022-03-30 20:22:23 -0700

    update2

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,8 +1,8 @@
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (
-        document.body.scrollTop > 650
-        || document.documentElement.scrollTop > 650
+        document.body.scrollTop > 200
+        || document.documentElement.scrollTop > 200
     ) {
         document.getElementById("navbar").style.background = "rgba(0,0,0,.75)";
     } else {
